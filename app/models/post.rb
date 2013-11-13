c0db94ca07d958a9ff3a30b4a951f54482b181ee
Charles <charleszivko@gmail.com>
2013-11-12 17:00:23 -0800

    slugify title

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ActiveRecord::Base
+
+  after_validation :slugify_title
+
+  protected
+  def slugify_title
+    self.slug = title.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+  end
 end
