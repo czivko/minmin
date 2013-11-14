b29ed01c9ad4d36de88ed9999aeb30f2753f814b
Charles <charleszivko@gmail.com>
2013-11-14 14:24:25 -0800

    post types and validation added

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -4,4 +4,8 @@ module PostsHelper
     post ||= @post
     post.tags.map { |f| "##{f}" }.join(" ") if post and post.tags
   end
+
+  def post_types_select
+    Post.post_types.map { |p| [p.to_s.titleize, p ] }
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,20 @@
 class Post < ActiveRecord::Base
-
+  POST_TYPES = [ :text, :photo, :quote, :link, :video ]
+  validates_inclusion_of :post_type, in: POST_TYPES
+  
+  before_validation :symbolize_usertype
   after_validation :slugify_title
+   
+  def self.post_types
+    POST_TYPES
+  end
 
   protected
   def slugify_title
     self.slug = title.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+  end
+
+  def symbolize_usertype
+    self.post_type = self.post_type.to_sym if post_type
   end
 end
