131adfa7d0ffc633831ddab6e6e90cbc53546196
Charles <charleszivko@gmail.com>
2013-11-17 10:54:13 -0800

    slug should be unique

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,9 @@
 class Post < ActiveRecord::Base
   POST_TYPES = [ :text, :photo, :quote, :link, :video ]
   validates_inclusion_of :post_type, in: POST_TYPES
-  
-  before_validation :symbolize_usertype
-  after_validation :slugify_title
+  validates_uniqueness_of :slug
+
+  before_validation :symbolize_usertype,:slugify_title
    
   def self.post_types
     POST_TYPES
