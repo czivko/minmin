94b9b32d38bd227f39e039cbb1369986d4139de0
Charles <charleszivko@gmail.com>
2013-11-17 14:30:26 -0800

    no hash tags and use commas when editing tags

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -5,6 +5,10 @@ module PostsHelper
     post.tags.map { |f| "##{f}" }.join(" ") if post and post.tags
   end
 
+  def edit_tags
+    @post.tags.map { |f| "#{f}" }.join(", ") if @post and @post.tags
+  end
+
   def post_types_select
     Post.post_types.map { |p| [p.to_s.titleize, p ] }
   end
